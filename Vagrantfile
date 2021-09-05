abab958358c34d68ea9256746f875cf1bd517a10
Rui Lopes <rgl@ruilopes.com>
2021-09-05 07:56:12 +0100

    install the cert-manager chart 1.5.3

--- Vagrantfile
+++ Vagrantfile
@@ -7,6 +7,7 @@ CONFIG_HELM_VERSION = 'v3.6.3' # see https://github.com/helm/helm/releases
 CONFIG_K9S_VERSION = 'v0.24.15' # see https://github.com/derailed/k9s/releases
 CONFIG_METALLB_CHART_VERSION = '2.5.4' # see https://artifacthub.io/packages/helm/bitnami/metallb
 CONFIG_EXTERNAL_DNS_CHART_VERSION = '5.4.5' # see https://artifacthub.io/packages/helm/bitnami/external-dns
+CONFIG_CERT_MANAGER_CHART_VERSION = '1.5.3' # see https://artifacthub.io/packages/helm/cert-manager/cert-manager
 CONFIG_TRAEFIK_CHART_VERSION = '10.3.2' # see https://artifacthub.io/packages/helm/traefik/traefik
 
 CONFIG_PANDORA_BRIDGE_NAME = nil
---
+++ provision-chart-cert-manager.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+source /vagrant/lib.sh
+
+cert_manager_chart_version="${1:-1.5.3}"; shift || true
+
+# provision cert-manager.
+# NB YOU MUST INSTALL CERT-MANAGER TO THE cert-manager NAMESPACE. the CRDs have it hard-coded.
+# NB YOU CANNOT INSTALL MULTIPLE INSTANCES OF CERT-MANAGER IN A CLUSTER.
+# NB the CRDs have to be installaled separately from the chart.
+# TODO would it make sense to have a separate helm chart for installing the CRDs?
+# see https://artifacthub.io/packages/helm/cert-manager/cert-manager
+# see https://github.com/jetstack/cert-manager/tree/master/deploy/charts/cert-manager
+# see https://cert-manager.io/docs/installation/supported-releases/
+# see https://cert-manager.io/docs/configuration/selfsigned/#bootstrapping-ca-issuers
+# see https://cert-manager.io/docs/usage/ingress/
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+kubectl apply -f "https://github.com/jetstack/cert-manager/releases/download/v$cert_manager_chart_version/cert-manager.crds.yaml"
+helm install cert-manager \
+  --namespace cert-manager \
+  --version "$cert_manager_chart_version" \
+  --create-namespace \
+  --wait \
+  jetstack/cert-manager
+kubectl apply -f - <<'EOF'
+---
+# see https://cert-manager.io/docs/reference/api-docs/#cert-manager.io/v1.ClusterIssuer
+apiVersion: cert-manager.io/v1
+kind: ClusterIssuer
+metadata:
+  name: selfsigned
+spec:
+  selfSigned: {}
+---
+# see https://cert-manager.io/docs/reference/api-docs/#cert-manager.io/v1.Certificate
+apiVersion: cert-manager.io/v1
+kind: Certificate
+metadata:
+  name: ingress
+  namespace: cert-manager
+spec:
+  isCA: true
+  subject:
+    organizations:
+      - Example
+    organizationalUnits:
+      - Kubernetes
+  commonName: Kubernetes Ingress
+  privateKey:
+    algorithm: ECDSA # NB Ed25519 is not yet supported by chrome 93 or firefox 91.
+    size: 256
+  duration: 8h # NB this is so low for testing purposes. default is 2160h (90 days).
+  secretName: ingress-tls
+  issuerRef:
+    name: selfsigned
+    kind: ClusterIssuer
+    group: cert-manager.io
+---
+# see https://cert-manager.io/docs/reference/api-docs/#cert-manager.io/v1.ClusterIssuer
+apiVersion: cert-manager.io/v1
+kind: ClusterIssuer
+metadata:
+  name: ingress
+spec:
+  ca:
+    secretName: ingress-tls
+EOF
