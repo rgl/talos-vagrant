ccebba615baa7ebeb3d4ae57281e41f2ec215346
Rui Lopes <rgl@ruilopes.com>
2021-10-26 04:59:52 +0100

    upgrade to talos 0.13.1

--- provision-talos.sh
+++ provision-talos.sh
@@ -3,7 +3,7 @@ source /vagrant/lib.sh
 
 
 dns_domain="$(hostname --domain)"
-talos_version="${1:-0.13.0}"; shift || true
+talos_version="${1:-0.13.1}"; shift || true
 kubernetes_version="${1:-1.22.2}"; shift || true
 control_plane_vip="${1:-10.10.0.3}"; shift || true
 
