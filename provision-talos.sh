f8a66822afb7c9ce26a81538129fc4c90d932593
Rui Lopes <rgl@ruilopes.com>
2021-11-18 10:05:42 +0000

    upgrade to talos 0.14.0-alpha.1

--- provision-talos.sh
+++ provision-talos.sh
@@ -3,7 +3,7 @@ source /vagrant/lib.sh
 
 
 dns_domain="$(hostname --domain)"
-talos_version="${1:-0.14.0-alpha.0}"; shift || true
+talos_version="${1:-0.14.0-alpha.1}"; shift || true
 kubernetes_version="${1:-1.22.3}"; shift || true
 control_plane_vip="${1:-10.10.0.3}"; shift || true
 pandora_ip_address="$(jq -r .CONFIG_PANDORA_IP /vagrant/shared/config.json)"
