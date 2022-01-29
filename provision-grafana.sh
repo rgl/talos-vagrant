9c0a38386339f3edbe34ee9d94bd45f1a49eb8f8
Rui Lopes <rgl@ruilopes.com>
2022-01-29 16:14:00 +0000

    configure grafana to autologin as admin

--- provision-grafana.sh
+++ provision-grafana.sh
@@ -24,5 +24,8 @@ docker run \
     --restart unless-stopped \
     --name grafana \
     -p 3000:3000 \
+    -e GF_AUTH_ANONYMOUS_ENABLED=true \
+    -e GF_AUTH_ANONYMOUS_ORG_ROLE=Admin \
+    -e GF_AUTH_DISABLE_LOGIN_FORM=true \
     -v $PWD/datasources:/etc/grafana/provisioning/datasources \
     grafana/grafana:$grafana_version
