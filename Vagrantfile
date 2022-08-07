e7949486edc1485de913979fc46a2c37296a1ffb
Rui Lopes <rgl@ruilopes.com>
2022-08-07 09:24:00 +0100

    upgrade to traefik chart 10.24.0 (app version 2.8.0)

--- Vagrantfile
+++ Vagrantfile
@@ -9,7 +9,7 @@ CONFIG_K9S_VERSION = 'v0.26.3' # see https://github.com/derailed/k9s/releases
 CONFIG_METALLB_CHART_VERSION = '0.13.4' # see https://github.com/metallb/metallb/releases
 CONFIG_EXTERNAL_DNS_CHART_VERSION = '6.7.4' # see https://artifacthub.io/packages/helm/bitnami/external-dns
 CONFIG_CERT_MANAGER_CHART_VERSION = '1.9.1' # see https://artifacthub.io/packages/helm/cert-manager/cert-manager
-CONFIG_TRAEFIK_CHART_VERSION = '10.15.0' # see https://artifacthub.io/packages/helm/traefik/traefik
+CONFIG_TRAEFIK_CHART_VERSION = '10.24.0' # see https://artifacthub.io/packages/helm/traefik/traefik
 CONFIG_KUBERNETES_DASHBOARD_CHART_VERSION = '5.7.0' # see https://artifacthub.io/packages/helm/k8s-dashboard/kubernetes-dashboard
 
 CONFIG_PANDORA_BRIDGE_NAME = nil
--- provision-chart-traefik.sh
+++ provision-chart-traefik.sh
@@ -5,16 +5,16 @@ source /vagrant/lib.sh
 # see https://artifacthub.io/packages/helm/traefik/traefik
 # see https://github.com/traefik/traefik-helm-chart
 # see https://github.com/traefik/traefik-helm-chart/blob/master/traefik/values.yaml
-traefik_chart_version="${1:-10.15.0}"; shift || true
+traefik_chart_version="${1:-10.24.0}"; shift || true
 domain="$(hostname --domain)"
 
-# add the gitlab helm charts repository.
+# add the traefik helm charts repository.
 helm repo add traefik https://helm.traefik.io/traefik
 helm repo update
 
 # search the chart and app versions, e.g.: in this case we are using:
 #     NAME             CHART VERSION  APP VERSION  DESCRIPTION
-#     traefik/traefik  10.15.0         2.6.1        A Traefik based Kubernetes ingress controller
+#     traefik/traefik  10.24.0        2.8.0        A Traefik based Kubernetes ingress controller
 helm search repo traefik/traefik --versions | head -5
 
 # install.
